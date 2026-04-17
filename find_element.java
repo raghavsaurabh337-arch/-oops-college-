8914ca55ae1b65f6f56343c14eb20cf3eb7c58a5
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-17 19:11:47 +0530

    find element program create

---
+++ find_element.java
@@ -0,0 +1,20 @@
+public 
+class find_element {
+
+    public static void main(String args[]) {
+        int arr[] = {3, 43, 65, 87, 90};
+        int i;
+        int n = 625;
+        for (i = 0; i < arr.length; i++) {
+            if (n == arr[i]) {
+                System.out.println("find tha number: "+arr[i]);
+                break;
+            }
+        }
+        if(i==arr.length){
+            System.out.println("do not find"+n);
+        }
+
+    }
+}
+
