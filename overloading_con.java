aad2d4cad490a631ada05876d00c2e08084fe179
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-18 22:32:45 +0530

    overloading method

--- overloading_con.java
+++ overloading_con.java
@@ -1,1 +1,26 @@
-class 
+class  overloading{
+     void show(){
+          System.out.println("this is first method" );
+     
+     }
+     void show(int a){
+          System.out.println("this is second method A : " + a);
+     
+     }
+     void show(int a ,int b){
+          System.out.println("this is third method A & b : " + a+" "+b);
+     
+     }
+    
+}
+
+class overloading_con{
+     public static void main(String[] args) {
+          overloading obj=new overloading();
+          obj.show();
+          obj.show(10);
+          obj.show(10,20);
+
+         
+     }
+}
--- private_constructor.java
+++ private_constructor.java
@@ -1,25 +1,24 @@
-private class private_const{
+class private_const {
      int a;
      double b;
      String c;
-    private_const() {
-     a=10;
-     b=900.98;
-     c="Saurabh Raghav";
 
-        System.out.println(" this is private constructor" +a+" "+b+" "+c);
-        
-    }
+     private private_const() {
+          a = 10;
+          b = 900.98;
+          c = "Saurabh Raghav";
 
-     
+          System.out.println("this is private constructor " + a + " " + b + " " + c);
+     }
+
+     //static method to create object
+     public static private_const createObject() {
+          return new private_const();
+     }
 }
 
 public class private_constructor {
      public static void main(String[] args) {
-          private_const obj = new private_const();
-         
-
-         
+          private_const obj = private_const.createObject(); 
      }
-     
 }
