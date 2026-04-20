31a8b87275073cb69ece79c3eb4d4e54b95d9369
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-20 23:22:11 +0530

    Array Question

---
+++ insert_ele_array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+public class insert_ele_array {
+     public static void main(String[] args);{
+          Scanner sc = new Scanner(System.in);
+         System.out.println("enter the array");   
+         int[] num = sc.nextInt();
+
+
+          System.out.println();
+
+        
+
+     }
+}
