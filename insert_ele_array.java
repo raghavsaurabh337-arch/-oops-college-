c0f6a61c18698876dd4c18167ce48feac088a503
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-21 21:33:12 +0530

    input array print

--- insert_ele_array.java
+++ insert_ele_array.java
@@ -1,15 +1,16 @@
-import java.util.*;
+
 import java.util.Scanner;
-public class insert_ele_array {
-     public static void main(String[] args);{
-          Scanner sc = new Scanner(System.in);
-         System.out.println("enter the array");   
-         int[] num = sc.nextInt();
-
-
-          System.out.println();
-
-        
-
+public class  insert_ele_array{
+     public static  void main(String[] args){
+          Scanner sc= new Scanner(System.in);
+          int n =sc.nextInt();
+          int[] num = new int[n];
+          System.out.println("enter the element of size:");
+          for(int i=0;i<=5;i++){
+               num[i]=sc.nextInt();
+          }
+          for(int i=0;i<5;i++){
+               System.out.println(num[i]+"");
+          }
      }
 }
