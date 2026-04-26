42cd5a44a1f89649faac3528e29a5f57481e439e
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-26 09:45:49 +0530

    program execute

--- exception_handle.java
+++ exception_handle.java
@@ -1,3 +1,19 @@
+
 public class exception_handle {
-     
+
+    public static void main(String[] args) {
+        try {
+            int a = 50, b = 0, c;
+            c = a / b;
+            System.out.println(c);
+          }
+          catch(ArithmeticException e) {
+              System.out.println(e);
+          }
+          finally{
+               System.out.println("Always execute ");
+          }
+         
+    }
+
 }
