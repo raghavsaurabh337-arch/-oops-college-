b53d4204400f601a560d5d3b4119585b89b31f5e
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-27 05:27:48 +0530

    interface program

--- interfaces.java
+++ interfaces.java
@@ -1,3 +1,21 @@
+
+import java.util.Random;
+
+interface pencard{
+     void pen_card();
+}
+class adharcard implements pencard{
+     public void pen_card(){
+          Random r =new Random();
+         long num = 100000000000L + (long)(r.nextDouble() * 900000000000L);
+
+          System.out.println("Random 12 Digit Number:: "+num);
+     }
+}
 public class interfaces {
+     public static void main(String[] args) {
+          adharcard obj=new adharcard();
+          obj.pen_card();
+     }
 
 }
