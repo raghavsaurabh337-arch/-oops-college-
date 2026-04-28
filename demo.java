1cd37283af126cfd9e663256ec41c7adb7b3784e
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-28 20:40:34 +0530

    28/04

---
+++ demo.java
@@ -0,0 +1,13 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Daemon Thread running");
+    }
+}
+
+public class demo{
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.setDaemon(true);   // make daemon
+        t1.start();
+    }
+}
