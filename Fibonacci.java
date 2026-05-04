390c7c37d44a38ed2cccf1aab238f23e347417cf
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-05-04 09:50:18 +0530

    Fibonacci program array using

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Fibonacci {
+     public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+
+          System.out.println("Enter the number:");
+          int num = sc.nextInt();
+          for(int i=0;i<=num;i++){
+               System.out.print(" "+i);
+          }
+
+     }
+     
+}
