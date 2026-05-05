1f2503d87eba9dcf2da27b6a5f1db255e102e23d
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-05-05 22:09:10 +0530

    change code

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+
 public class Fibonacci {
-     public static void main(String[] args){
-          Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-          System.out.println("Enter the number:");
-          int num = sc.nextInt();
-          for(int i=0;i<=num;i++){
-               System.out.print(" "+i);
-               
-          }
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
 
-     }
-     
+        int a = 0, b = 1;
+
+        System.out.print(a + " " + b);
+
+        for(int i = 2; i < num; i++){
+            int c = a + b;
+            System.out.print(" " + c);
+
+            a = b;
+            b = c;
+        }
+    }
 }
