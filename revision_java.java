a89df1da88fcf7306eb5cd34e8e6872b98b9b9d2
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-05-16 22:05:34 +0530

    revision java

---
+++ revision_java.java
@@ -0,0 +1,16 @@
+class varible{
+     int b=10; //goble varibles
+     static int c=10; // static goble varibles
+
+     void fun(){
+          int a=10; // local varibles
+          static int d=10; //static  local varibles
+     }
+}
+public class revision_java{
+     public static void main(String[] args){
+          
+
+
+     }
+}
